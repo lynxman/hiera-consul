41426f10c3509f6ec995b53101856bf531870636
Marc Cluet <lynxman@lynxman.net>
2014-08-02 16:51:45 +0100

    Remade parse_result and called the right path

--- lib/hiera/backend/consul_backend.rb
+++ lib/hiera/backend/consul_backend.rb
@@ -1,4 +1,4 @@
-# Hiera backend for the etcd distributed configuration service
+# Hiera backend for Consul
 class Hiera
   module Backend
     class Consul_backend
@@ -48,7 +48,7 @@ class Hiera
             Hiera.debug("[hiera-consul]: The specified path #{path}/#{key} is malformed, skipping")
             next
           end
-          httpreq = Net::HTTP::Get.new(path)
+          httpreq = Net::HTTP::Get.new("#{path}/#{key}")
           begin
             result = @consul.request(httpreq)
           rescue Exception => e
@@ -58,56 +58,28 @@ class Hiera
           end
           unless result.kind_of?(Net::HTTPSuccess)
             Hiera.debug("[hiera-consul]: bad http response from #{@config[:host]}:#{@config[:port]}#{path}")
-            Hiera.debug("HTTP response code was #{result.code}")
-            unless ( result.code == '404' && @config[:ignore_404] == true )
-              raise Exception, 'Bad HTTP response' unless @config[:failure] == 'graceful'
-            end
+            Hiera.debug("[hiera-consul]: HTTP response code was #{result.code}")
             next
           end
-
-          answer = self.parse_result(result.body, resolution_type, scope)
+          Hiera.debug("[hiera-consul]: Answer was #{result.body}")
+          answer = self.parse_result(result.body)
           next unless answer
           break
         end
         answer
       end
 
-
-      def parse_result(res, type, scope)
+      def parse_result(res)
+        require 'base64'
         answer = nil
-        case type
-        when :array
-          answer ||= []
-          begin
-            data = Backend.parse_answer(JSON[res], scope)
-          rescue
-            Hiera.warn("[hiera-consul]: '#{res}' is not in json format, and array lookup is requested")
-            return answer
-          end
-
-          if data.is_a?(Array)
-            answer = data.clone
-          else
-            Hiera.warn("Data is in json format, but this is not an array")
-          end
-        when :hash
-          answer ||= {}
-          begin
-            data = Backend.parse_answer(JSON[res], scope)
-          rescue
-            Hiera.warn("[hiera-consul]: '#{res}' is not in json format, and hash lookup is requested")
-            return answer
-          end
-          if data.is_a?(Hash)
-            answer = data.clone
-          else
-            Hiera.warn("Data is in json format, but this is not an hash")
-          end
-        else
-          answer = Backend.parse_answer(res, scope)
+        # Consul always returns an array
+        res_array = JSON.parse(res)
+        if res_array.is_a?(Array)
+          answer = Base64.decode64(res_array.first['Value'])
         end
         answer
       end
+
     end
   end
 end
